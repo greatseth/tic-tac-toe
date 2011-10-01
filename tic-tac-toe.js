f2ecd4f9376b698a1d674679e10afb3998b38a30
Seth Thomas Rasmussen <sethrasmussen@gmail.com>
2011-10-01 16:14:23 -0400

    Basic behavior for marking cells
    - Game keeps track of current turn
    - Clicking cells marks X or O appropriately,
      but no checks for win conditions, yet

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,16 +2,48 @@ var TTT = {
   current_game: null,
 
   Game: function() {
-    this.board = $('#board');
-    this.cells = this.board.find('li');
+    var game = this;
 
-    this.cells.click(function(e) {
-      console.log(e);
+    game.turn  = 1;
+    game.board = $('#board');
+    game.cells = game.board.find('li');
+
+    game.cells.click(function(e) {
+      var c = $(e.target);
+
+      if (!game.mark(c)) {
+        game.mark(c, game.x_turn() ? "X" : "O");
+        game.advance_turn();
+      } else {
+        return;
+      }
     });
   }
 };
 
-// TTT.Game.prototype = { };
+TTT.Game.prototype = {
+  o_turn: function() {
+    return ((this.turn % 2) == 0);
+  },
+
+  x_turn: function() {
+    return !(this.o_turn());
+  },
+
+  mark: function(cell, mark) {
+    if (mark) {
+      cell.text(mark);
+    } else {
+      cell.text();
+    }
+
+    return (cell.text() !== "");
+  },
+
+  advance_turn: function() {
+    this.turn = this.turn + 1;
+  }
+};
 
 window.onload = function() {
   TTT.current_game = new TTT.Game();
